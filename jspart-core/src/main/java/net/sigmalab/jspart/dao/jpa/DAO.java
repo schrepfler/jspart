52440d5eb5eeb8f2eebca940da4ac62dd69d998e
schrepfler <schrepfler@825bd29f-1526-0410-9f58-03c760f7af5a>
2007-03-16 23:59:45 +0000

    modified   src/main/java/net/sigmalab/jspart/dao/jpa/DAO.java Modified save() method, no longer doing test in order to check whether to use persist() or merge() as a discussion on the spring forums indicated merge will always do the right thing.
    deleted    src/test/java/net/sigmalab/jspart/dao/jpa/ArtistDAOTest.java
    remainder from the artist-author refactoring.
    
    added      src/test/java/net/sigmalab/jspart/dao/jpa/AuthorDAOTest.java
    Updated the test code.
    
    added      src/test/resources/META-INF/persistence.xml
    forgotten, now added.

--- jspart-core/src/main/java/net/sigmalab/jspart/dao/jpa/DAO.java
+++ jspart-core/src/main/java/net/sigmalab/jspart/dao/jpa/DAO.java
@@ -2,42 +2,35 @@ package net.sigmalab.jspart.dao.jpa;
 
 import java.io.Serializable;
 import java.util.List;
-
 import org.springframework.orm.jpa.support.JpaDaoSupport;
 import org.springframework.transaction.annotation.Transactional;
-
 import net.sigmalab.jspart.dao.IDAO;
 
 @Transactional
 public class DAO<T, PK extends Serializable> extends JpaDaoSupport implements IDAO<T, PK> {
-
-	private Class<T> persistentClass;
-	
-	public DAO(Class<T> persistentClass){
-		this.persistentClass = persistentClass;
-	}
-	
-	public T get(Serializable id) {
-		return getJpaTemplate().find(this.persistentClass, id);
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<T> getAll() {
-		return getJpaTemplate().find("from "+this.persistentClass.getName());
-	}
-
-	public void remove(PK id) {
-		getJpaTemplate().remove(this.get(id));
-	}
-
-	public T save(T object) {
-		if (getJpaTemplate().contains(object)) {
-			getJpaTemplate().merge(object);
-		} else {
-			getJpaTemplate().persist(object);
-		}
-		getJpaTemplate().flush();
-		return object;
-	}
-
+    
+    private Class<T> persistentClass;
+    
+    public DAO(Class<T> persistentClass){
+        this.persistentClass = persistentClass;
+    }
+    
+    public T get(Serializable id) {
+        return getJpaTemplate().find(this.persistentClass, id);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<T> getAll() {
+        return getJpaTemplate().find("from "+this.persistentClass.getName());
+    }
+    
+    public void remove(PK id) {
+        getJpaTemplate().remove(this.get(id));
+    }
+    
+    public T save(T object) {
+        object = getJpaTemplate().merge(object);
+        return object;
+    }
+    
 }
